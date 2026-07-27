e04e6c5b1337d6e6f16a822cf20a5c262087db76
Matthew Payne <mattspycrafter@gmail.com>
2026-07-27 14:23:24 -0400

    Part 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,15 +4,25 @@ import Total from "./Total.jsx"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const parts = [part1, part2, part3] 
 
   return (
     <div>
       <Header course={course}/>
-      <Content/>
-      <Total total={exercises1 + exercises2 + exercises3}/> 
+      <Content parts={[part1, part2, part3]}/>
+      <Total total={parts.reduce((acc, val) => acc + val.exercises, 0)}/> 
     </div>
   )
 }
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,20 +1,14 @@
 import Part from "./Part.jsx"
 
-const Content = () => {
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
-
+const Content = ({parts}) => {
+    const [part1, part2, part3] = parts 
+    
     return(
-    <div>        
-        <Part name={part1} exercises={exercises1}/>
-        <Part name={part2} exercises={exercises2}/>
-        <Part name={part3} exercises={exercises3}/>
-    </div>  
+        <div>        
+            <Part name={part1.name} exercises={part1.exercises}/>
+            <Part name={part2.name} exercises={part2.exercises}/>
+            <Part name={part3.name} exercises={part3.exercises}/>
+        </div>  
     )
 } 
 
