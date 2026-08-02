729d763d4dc63000cf8ed6fa0248151e2464cb25
Matthew Payne <mattspycrafter@gmail.com>
2026-08-02 13:57:33 -0400

    Part 1.11

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -8,15 +8,21 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <>
+    <div>
+
       <h2>Statistics</h2>
-      <StatisticLine text={"good"} value={good} />
-      <StatisticLine text={"neutral"} value={neutral} />
-      <StatisticLine text={"bad"} value={bad} />
-      <StatisticLine text={"total"} value={total()} />
-      <StatisticLine text={"average"} value={total() === 0 ? 0 : (good - bad) / total()} />
-      <StatisticLine text={"positive"} value={(total() === 0 ? 0 : (good) / total()) * 100} />
-    </>
+      <table>
+        <tbody>
+          <StatisticLine text={"good"} value={good} />
+          <StatisticLine text={"neutral"} value={neutral} />
+          <StatisticLine text={"bad"} value={bad} />
+          <StatisticLine text={"total"} value={total()} />
+          <StatisticLine text={"average"} value={total() === 0 ? 0 : (good - bad) / total()} />
+          <StatisticLine text={"positive"} value={(total() === 0 ? 0 : (good) / total()) * 100} />
+        </tbody>
+      </table>
+
+    </div>
   )
 
 }
