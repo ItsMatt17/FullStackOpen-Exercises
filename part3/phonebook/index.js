9a73a83a1c54c13e08cd91829652d15730ffc5ec
Matthew Payne <mattspycrafter@gmail.com>
2026-08-21 17:56:55 -0400

    Part 3.5

---
+++ part3/phonebook/index.js
@@ -0,0 +1,92 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+
+let persons = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+
+app.get("/api/info", (_, resp) => {
+  resp.send(
+    `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date().toString()}</p>
+    </div>`
+  )
+})
+
+app.get("/api/persons", (_, resp) => resp.json(persons))
+
+app.get("/api/persons/:id", (req, resp) => {
+  const id = req.params.id
+  const person = persons.find((p) => p.id === id)
+
+  if (person) return resp.json(person)
+
+  resp.status(404)
+  resp.json({ error: `Could not find user with id of ${id}` })
+})
+
+app.delete("/api/persons/:id", (req, resp) => {
+  console.log("here")
+  id = req.params.id
+  persons = persons.filter((p) => p.id !== id)
+
+  resp.status(204).end()
+})
+
+app.post("/api/persons", (req, resp) => {
+
+  const body = req.body
+  if ((isEmpty(body)) || (typeof body?.name !== "string") || (typeof body?.number !== "string")) {
+    console.log("in if statement.")
+    return resp.status(400).json({
+      error: "Invalid request body."
+    })
+  }
+
+  const person = {
+    "id": new String(Math.floor(1000 * Math.random())),
+    "name": body.name,
+    "number": body.number,
+  }
+
+  if (persons.find((p) => p.id === person.id)) {
+    console.log(`User ID of ${person.id} matched with another person!`)
+  }
+
+  persons.push(person)
+  resp.json(person)
+
+})
+
+
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0
+}
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
